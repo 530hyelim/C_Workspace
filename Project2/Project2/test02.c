0d2dbb2309e8377c6b6bfbaac368c6abfc08e3e9
530hyelim <530hyelim@gmail.com>
2025-05-07 23:00:11 +0900

    May7

--- Project2/Project2/test02.c
+++ Project2/Project2/test02.c
@@ -14,6 +14,10 @@ Data의 종류(Type)
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+// 정수 타입 변수의 최소값과 최대값을 쉽게 구할 수 있음(대소문자에 주의)
+#include <float.h>
+// 실수 타입 변수의 최소값과 최대값을 쉽게 구할 수 있음
 
 int main(void) {
 	printf("문자열 데이터 출력\n");
@@ -57,6 +61,104 @@ int main(void) {
 	printf("%d - %d = %d\n", data1, data2, data1 - data2);
 	printf("%d * %d = %d\n", data1, data2, data1 * data2);
 	system("pause");
+
+	/*
+	정수 Type 변수의 종류 (표현할 수 있는 수의 크기에 따라)
+		- short: 2byte (16bit)만을 사용하여 수를 표현(-2^15 ~ 2^15-1)
+			1bit는 음수/양수를 표현하는 부호 비트
+			연산 속도가 int보다 느림
+		- int: 4byte (32bit)를 사용하여 수를 표현(-2^31 ~ 2^31-1)
+			정수 연산의 기본 단위
+			연산 속도가 가장 빠르다는 장점
+		- long: 4byte (32bit를 사용하여 수를 표현(-2^31 ~ 2^31-1)
+			printf함수의 서식 문자: %ld
+			int를 2바이트로 취급하는 컴퓨팅 시스템에서 4바이트 정수 공간을 확보하기 위해 사용
+		- long long: 8byte (64bit) 사용하여 수를 표현(-2^63 ~ 2^63-1)
+			printf함수의 서식 문자: %lld
+			엄청나게 큰 값을 저장하는 용도로 사용
+	*/
+	short sVar1 = 32767;
+	short sVar2 = 50000; // 출력하면 엉뚱한 값이 나옴
+	int iVar1 = 2147483647;
+	int iVar2 = 2200000000; // 출력하면 엉뚱한 값
+	long lVar1 = 2147483647;
+	long lVar2 = 2200000000; // 엉뚱한 값
+	// long type의 변수는 int가 4바이트 정수를 표현하지 못하는 컴퓨터에서(예전 16비트 이하 컴퓨터)
+	// 명시적으로 4바이트의 정수를 표현하기 위해 사용됨
+	long long llVar1 = 9220000000000000000;
+	long long llVar2 = 9300000000000000000; // 엉뚱
+	printf("%lld\n", llVar2);
+	system("pause");
+
+	/*
+	unsigned
+		음의 정수를 사용하지 않고, 음의 정수를 표현하는 만큼, 양의 정수로 표현하는 것
+		short(-32768 ~ 32767) / unsigned short(0 ~ 65535)
+		int(-2147483648 ~ 2147483647) / unsigned int(0 ~ 4294967295)
+	*/
+	unsigned short usVar1 = 65535;
+	unsigned short usVar2 = 70000;
+	unsigned int uiVar1 = 4294967295;
+	unsigned int uiVar2 = 4300000000;
+	unsigned long ulVar1 = 4294967295;
+	unsigned long ulVar2 = 4300000000;
+	unsigned long long ullVar1 = 18446744073709551615;
+	//unsigned long long ullVar2 = 18446744073709551616; => 빌드 실패!
+	printf("%lu : %lu\n", ulVar1, ulVar2);
+	system("pause");
+
+	printf("short min : %d, max : %d\n\n", SHRT_MIN, SHRT_MAX);
+	printf("int min : %d, max : %d\n\n", INT_MIN, INT_MAX);
+	printf("long min : %ld, max : %ld\n\n", LONG_MIN, LONG_MAX);
+	printf("long long min : %lld, max : %lld\n\n", LLONG_MIN, LLONG_MAX);
+
+	printf("unsigned short max : %u\n\n", USHRT_MAX);
+	printf("unsigned int max : %u\n\n", UINT_MAX);
+	printf("unsigned long max : %lu\n\n", ULONG_MAX);
+	printf("unsigned long long max : %llu\n\n", ULLONG_MAX);
+	// unsigned의 최소값은 항상 0이기 때문에 출력X
+	system("pause");
+
+	/*
+	실수 Type 변수의 종류 (표현할 수 있는 수의 크기에 따라)
+		- float: 4byte (32bit)만을 사용하여 수를 표현
+			1bit는 부호비트(+/-)
+			8bit는 지수부(정수 표현), 23bit는 가수부(소수점 이하)
+		- double: 8byte (64bit)를 사용하여 수를 표현
+			1bit는 부호비트
+			11bit는 지수부, 52bit는 가수부
+		- long double: double보다 더 큰 수의 표현이 필요할 때, 상황에 따라 다르게 쓰임
+	*/
+	printf("float min : %e, max : %e\n\n", FLT_MIN, FLT_MAX);
+	// %e는 10의 승수로 출력하는 서식 문자
+	double dMin = DBL_MIN, dMax = DBL_MAX;
+	printf("double min : %e, max : %e\n\n", dMin, dMax);
+	system("pause");
+
+	/*
+	char
+		메모리를 1byte(8bit) 사용하여 정수와 문자를 표현(%c or %d)
+		1bit는 부호비트, -2^7 ~ 2^7-1까지의 수를 표현(-128 ~ 127)
+		ASCII Code: 키보드 입력 값을 0~127의 정수로 처리, 문자에 대응
+		unsigned char: 0 ~ 255 (%u)
+	*/
+	char charVar1 = 'A', charVar2 = 66;
+	printf("charVar1: %c, %d\n\n", charVar1, charVar1); // A, 65
+	printf("charVar2: %c, %d\n\n", charVar2, charVar2); // B, 66
+	char chMin = CHAR_MIN, chMax = CHAR_MAX;
+	printf("char min: %d, max : %d\n\n", chMin, chMax); // -128, 127
+	unsigned char uChMax = UCHAR_MAX;
+	printf("unsigned char max : %d\n\n", chMax);
+	system("pause");
+
+	// sizeof : 변수가 몇 바이트의 메모리르 차지하는지 알려주는 연산자
+	char charVar = CHAR_MAX;
+	short shortVar = SHRT_MAX;
+	int intVar = INT_MAX;
+	long longVar = LONG_MAX;
+	long long longlongVar = LLONG_MAX;
+	int chSize = sizeof charVar, shSize = sizeof shortVar, iSize = sizeof intVar, lSize = sizeof longVar, llSize = sizeof longlongVar;
+	printf("%d, %d, %d, %d, %d\n\n", chSize, shSize, iSize, lSize, llSize);
 
 	return 0;
 }
---
+++ Project3/Project3/test03.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+	/*
+	상수
+		- 변수의 반대 개념(변하지 않는 수를 저장하는 공간)
+		- 저장된 공간에 새로운 값을 담을 수 없다
+		- 처음 대입된 값을 바꿀 수 없다
+
+	일반상수
+		- 일회성으로 재사용 불가
+		- 문자 상수: 4byte
+		- 정수 상수: 4byte
+		- 실수 상수: 8byte
+
+	const
+		- 재사용이 가능한 상수를 만드는 방법
+		- 저장 공간을 상수 공간으로 만드는 방법
+		- 선언 및 초기화된 저장 공간의 값이 변경되는 것을 막아주는 키워드
+		- 선언과 동시에 초기화 필요
+		- 선언 이후에 대입 연산을 통해 값 변경 불가
+		- 변경할 필요가 없는 고정된 값을 만들 때 사용
+		- 예) INT_MAX
+
+	상수의 이름
+		- 의미가 명확한 이름으로 만든다
+		- 모두 대문자
+		- 단어와 단어가 이어질 때는 _사용
+	*/
+	printf("%d + %d = %d\n\n", 9876, 8765, 9876 + 8765); 
+	printf("%d - %d = %d\n\n", 9876, 8765, 9876 - 8765);
+	// 일반상수는 위치값을 알지 못하기 때문에 메모리 공간의 값을 바꿀 수 없음
+
+	const double MARATHON_LENGTH = 42.195;
+	//MARATHON_LENGTH = 55.195;
+	//const를 붙인 순간 상수가 되기 때문에 값을 바꿀 수 없다
+	//변경될 필요가 없는 값을 재사용하기 위해 선언
+	printf("MARATHON_LENGTH : %lf\n\n", MARATHON_LENGTH);
+	printf("MARATHON_LENGTH 재사용 : %lf\n\n", MARATHON_LENGTH);
+
+	const MARATHON_LENGTH2;
+	//MARATHON_LENGTH2 = 42.195;
+	//const변수는 선언과 함께 값을 초기화 해야 함
+	system("pause");
+
+	/*
+	문자열(character string %s)
+		- string은 줄을 의미. 문자를 줄지어 놓았다는 뜻
+		- 문자 배열의 형태로 선언하고 초기화
+		- 선언된 크기보다 하나 작은 문자열 입력
+		- 문자열의 끝에는 문자열 종료 문자가 들어간다
+
+	문자열은 대입 연산이 불가능!
+		=> strcpy_s 함수를 사용해야 함
+		- 문자열의 초기화 이후에, 다시 문자열 입력이 필요할 때
+		- #include<string.h> 필요
+	*/
+	char chArray[30] = "nice to meet you, too.";
+	printf("문자열 변수 : %s, 상수 : %s\n\n", chArray, "일반 문자열 상수");
+
+	//chArray = "how are you???";
+	char chArray2[30];
+	//chArray2 = "how are you???";
+
+	strcpy_s(chArray, sizeof chArray, "how are you???");
+	printf("chArray : %s\n\n", chArray);
+	strcpy_s(chArray2, 30, "i am fine, thank you.");
+	printf("chArray2 : %s\n\n", chArray2);
+
+	system("pause");
+	return 0;
+}
